e20c368b838184999a80c5c5dad88b6f617bc5f4
jazhe <jazmin.h.e@hotmail.com>
2020-11-13 13:05:45 -0600

    Fin de las explicaciones del comando INSERT

--- reto1.sql
+++ reto1.sql
@@ -56,3 +56,24 @@ VALUES('Julio Cortazar', 'ARG'),
 ('Isabel Allende', 'CHI'),
 ('Octavio Paz', 'MEX'),
 ('Juan Carlos Onetti', 'URU');
+
+INSERT INTO `clients`(client_id, name, email, birthdate, gender, active) VALUES
+	(1,'Maria Dolores Gomez','Maria Dolores.95983222J@random.names','1971-06-06','F',1),
+	(2,'Adrian Fernandez','Adrian.55818851J@random.names','1970-04-09','M',1),
+	(3,'Maria Luisa Marin','Maria Luisa.83726282A@random.names','1957-07-30','F',1),
+	(4,'Pedro Sanchez','Pedro.78522059J@random.names','1992-01-31','M',1);
+
+INSERT INTO `clients`(name, email, birthdate, gender, active) 
+VALUES ('Pedro Sanchez','Pedro.78522059J@random.names','1992-01-31','M',0)
+ON DUPLICATE KEY UPDATE active = VALUES(active)
+
+INSERT INTO books(title, author_id) VALUES('El laberinto de la Soledad', 6);
+
+/*Usando un subquery*/
+INSERT INTO books(title, author_id, `year`)
+VALUES('Vuelta al Laberinto de la Soledad',
+    (SELECT author_id FROM authors 
+    WHERE name = 'Octavio Paz'
+    LIMIT 1
+    ), 1960
+);
